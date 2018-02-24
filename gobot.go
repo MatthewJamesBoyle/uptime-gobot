32107eb719b69a5a2f04e7a8de9eb44c2cdcfa1b
Matt Boyle <matthew.boyle@crowdcube.com>
2018-02-24 22:28:14 +0000

    v1 done :)

--- gobot.go
+++ gobot.go
@@ -12,12 +12,21 @@ import (
 	"mvdan.cc/xurls"
 )
 
-const down = " is down"
-const badStatus = " is returning a non 200 status code. Worth checking? "
+const (
+	Up        = iota
+	Down      = iota
+	BadStatus = iota
+)
 
 func init() {
 	gotenv.Load()
 
+}
+
+type trackedURL struct {
+	url   string
+	up    bool
+	timer int
 }
 
 func main() {
@@ -26,20 +35,25 @@ func main() {
 
 	urlsToMonitor := parseURLs(string(file))
 
-	c := make(chan string)
+	c := make(chan trackedURL)
 
 	for _, url := range urlsToMonitor {
 		go checkURL(url, c)
 	}
 
 	for l := range c {
-		go func(l string) {
+		go func(l trackedURL) {
 			time.Sleep(intervalInSeconds)
 			checkURL(l, c)
 		}(l)
 	}
 }
 
+// readArgs looks for the -file and -interval
+// flag in the command line when the program
+// is executed. if a file is not present, the
+// program will exit. if no interval is passed,
+// it defailts to 1 second.
 func readArgs() ([]byte, time.Duration) {
 	filePath := flag.String("file", "", "the filepath of the URLs you want to monitor")
 	i := flag.Int("interval", 1, "the gap between polls at your provided urls in seconds. 1 second by default")
@@ -54,29 +68,80 @@ func readArgs() ([]byte, time.Duration) {
 	return file, intervalInSeconds
 }
 
-func parseURLs(file string) []string {
-	urlsToMonitor := xurls.Strict().FindAllString(file, -1)
-	if len(urlsToMonitor) == 0 {
+// Takes the contents of the file
+// and parses all urls out of it.
+// If no urls are present, the program
+// will exit.
+func parseURLs(file string) []trackedURL {
+	urls := xurls.Strict().FindAllString(file, -1)
+	if len(urls) == 0 {
 		fmt.Println("No urls were found in the file you provided.")
 		os.Exit(1)
 	}
+	var urlsToMonitor []trackedURL
+	for _, url := range urls {
+		urlsToMonitor = append(urlsToMonitor, trackedURL{url, true, 0})
+	}
+
 	return urlsToMonitor
 }
 
-func checkURL(url string, c chan string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		Post(url, down)
-		c <- url
-		return
-	}
-	if resp.StatusCode != 200 {
-		Post(url, badStatus)
-		c <- url
-		return
-	}
+// checkURL takes a url and a channel
+// to pass the data into. It does a get
+// request to the url passed and if it
+// does not get a 200 status code, it
+// calls Post() and writes to Slack.
+func checkURL(tracked trackedURL, c chan trackedURL) {
 
-	c <- url
+	resp, err := http.Get(tracked.url)
+	if err != nil {
+		if tracked.up {
+			updateStatus(&tracked, false)
+		} else {
+			updateStatus(&tracked, false)
+
+			Post(tracked.url, Down, tracked.timer)
+
+		}
+		c <- tracked
+		return
+	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != 200 {
+		if tracked.up {
+			updateStatus(&tracked, false)
+			Post(tracked.url, BadStatus, tracked.timer)
+
+		} else {
+			updateStatus(&tracked, false)
+
+		}
+		c <- tracked
+		return
+	}
+
+	if !tracked.up {
+		updateStatus(&tracked, true)
+		Post(tracked.url, Up, tracked.timer)
+
+		return
+	}
+	updateStatus(&tracked, true)
+	c <- tracked
+
+}
+
+func updateStatus(tu *trackedURL, up bool) {
+	if up {
+		tu.up = true
+		tu.timer = 0
+	} else {
+		tu.up = false
+		tu.timer++
+	}
+}
+
+func (t trackedURL) print() {
+	fmt.Println(t)
 }
--- slack.go
+++ slack.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	slack "github.com/ashwanthkumar/slack-go-webhook"
 )
 
-func Post(url string, message string) {
+// Post takes a URL and a message. It then
+// posts to slack the URL and the message.
+// If the webhook url can't be found, calls
+// os.exit(1)
+func Post(url string, status int, timer int) {
 	webhookURL := os.Getenv("WEBHOOK_URL")
 	if webhookURL == "" {
 		fmt.Println("couldn't find a webhook url")
 		os.Exit(1)
 	}
 	payload := slack.Payload{
-		Text:      url + message,
+		Text:      getMessage(url, status, timer),
 		Username:  os.Getenv("SLACK_USERNAME"),
 		Channel:   os.Getenv("SLACK_CHANNEL"),
 		IconEmoji: ":cold_sweat:",
@@ -24,4 +29,17 @@ func Post(url string, message string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func getMessage(url string, status int, timer int) string {
+	var message string
+	switch status {
+	case Up:
+		message = url + " is back up after " + strconv.Itoa(timer) + " seconds."
+	case Down:
+		message = url + " is down."
+	case BadStatus:
+		message = "Recieving a bad status from " + url + ". Best check it out!"
+	}
+	return message
 }
